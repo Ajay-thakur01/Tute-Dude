2e8289ffbe485392d585b097df8b923d9894b9c7
Ajay thakur <at3855073@gmail.com>
2026-08-09 10:36:46 +0530

    All files added

---
+++ Frontend/JavaScript/datatypes, operaters/script.js
@@ -0,0 +1,32 @@
+   // let, cont, var
+        let name = "Ajay" ;
+        let age = "22" ;
+        let x = 69 ;
+        let isEligibleToVote =true ;
+        // console.log("My Name is "+name)
+        console.log("My Name is",name) ;
+        console.log("My age is",age);
+        console.log(isEligibleToVote) ;
+
+        let a = 10;
+        let b = 5;
+        let sum = a+b;
+        // console.log(a+b);
+        console.log(sum) ;
+        console.log(sum-a) ;
+
+        let varName = "Rohan" ;
+        console.log(varName) ;
+        varName = 'Intro to JS' ;
+        console.log(varName) ;
+        varName = 9 ;
+        console.log(varName) ;
+
+        const pet = "Jadu" ;
+        // pet = 'abc'
+        console.log(pet) ;
+
+        var college = "Arni University" ;
+        college = "811"
+        console.log(college) ;
+
---
+++ Frontend/Tasks/Ajay_Task6/Readme.md
@@ -0,0 +1,5 @@
+## How to Run
+
+1. Download or clone the project.
+2. Open the `index.html` file in any web browser.
+3. The website will load and display all sections.
